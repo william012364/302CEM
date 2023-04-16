df2cc7d88e189994bc97b2536d80da724cb40b35
william012364 <73682978+william012364@users.noreply.github.com>
2023-04-16 15:48:20 +0800

    Add files via upload

---
+++ test_tax.py
@@ -0,0 +1,494 @@
+from decimal import *
+import pytest
+
+
+class LessThanZeroException(Exception):
+    "Raised when the input value is less than 0"
+    pass
+
+
+def print_row(name, val1, val2, val3):
+    print("%-45s %18s %18s %18s" % (name, val1, val2, val3))
+
+
+basicA = Decimal("132000")
+# Basic Allowance
+mpA = Decimal("264000")
+# Married Person's Allowance
+
+
+def intro():
+    print("\nSimple HK Salaries Tax Calculator")
+    print("For year of assessment 2022/23")
+    print("The purpose of this program is to determine mandotory MPF contributions, as well as salaries tax to be paid under seperate or joint assessment")
+
+
+def brk():
+    print()
+    print("======================================================================================================")
+    print()
+
+
+def input1():
+    # take input from the user
+    while True:
+        try:
+            inc1 = Decimal(input("Enter annual income for person 1: "))
+            # Annual Income for Person 1
+            if inc1 < Decimal("0"):
+                raise LessThanZeroException
+        except InvalidOperation:
+            print("Invalid input. Please enter a valid number.")
+            continue
+        except LessThanZeroException:
+            print("Invalid input. Income cannot be less than 0.")
+            continue
+        break
+
+    return inc1
+
+
+def input2():
+    # take input from the user
+    while True:
+        try:
+            inc2 = Decimal(input("Enter annual income for person 2: "))
+            # Annual Income for Person 2
+            if inc2 < Decimal("0"):
+                raise LessThanZeroException
+        except InvalidOperation:
+            print("Invalid input. Please enter a valid number.")
+            continue
+        except LessThanZeroException:
+            print("Invalid input. Income cannot be less than 0.")
+            continue
+        break
+
+    return inc2
+
+
+def main(inc1, inc2):
+    minc1 = inc1/Decimal("12")
+    # Monthly Income for Person 1
+    minc2 = inc2/Decimal("12")
+    # Monthly Income for Person 2
+
+    if minc1 < Decimal("7100"):
+        mmpf1 = Decimal("0")
+    elif minc1 >= Decimal("7100") and minc1 <= Decimal("30000"):
+        mmpf1 = minc1*Decimal("0.05")
+    elif minc1 > Decimal("30000"):
+        mmpf1 = Decimal("1500")
+
+    mpf1 = mmpf1*Decimal("12")
+    # mmpf1 = Person 1 Monthly Mandotory MPF Contributions
+    # mpf1 = Person 1 Yearly Mandatory MPF Contributions
+
+    if minc2 < Decimal("7100"):
+        mmpf2 = Decimal("0")
+    elif minc2 >= Decimal("7100") and minc2 <= Decimal("30000"):
+        mmpf2 = minc2*Decimal("0.05")
+    elif minc2 > Decimal("30000"):
+        mmpf2 = Decimal("1500")
+
+    mpf2 = mmpf2*Decimal("12")
+    # mmpf2 = Person 2 Monthly Mandotory MPF Contributions
+    # mpf2 = Person 2 Yearly Mandatory MPF Contributions
+
+    jinc = inc1+inc2
+    # Joint Total Annual Income
+    jmpf = mpf1+mpf2
+    # Joint Total Annual Mandotory MPF Contributions
+
+    cinc1 = inc1 - mpf1 - basicA
+    if cinc1 < Decimal("0"):
+        cinc1 = Decimal("0")
+    # Person 1 Annual Chargeable Income
+
+    cinc2 = inc2 - mpf2 - basicA
+    if cinc2 < Decimal("0"):
+        cinc2 = Decimal("0")
+    # Person 2 Annual Chargeable Income
+
+    cjinc = jinc - jmpf - mpA
+    if cjinc < Decimal("0"):
+        cjinc = Decimal("0")
+    # Joint Annual Chargeable Income
+
+    if cinc1 <= Decimal("0"):
+        tax1 = Decimal("0")
+    elif cinc1 > Decimal("0") and cinc1 <= Decimal("50000"):
+        tax1 = cinc1*Decimal("0.02")
+    elif cinc1 > Decimal("50000") and cinc1 <= Decimal("100000"):
+        tax1 = Decimal("1000")+(cinc1-Decimal("50000"))*Decimal("0.06")
+    elif cinc1 > Decimal("100000") and cinc1 <= Decimal("150000"):
+        tax1 = Decimal("4000")+(cinc1-Decimal("100000"))*Decimal("0.1")
+    elif cinc1 > Decimal("150000") and cinc1 <= Decimal("200000"):
+        tax1 = Decimal("9000")+(cinc1-Decimal("150000"))*Decimal("0.14")
+    elif cinc1 > Decimal("200000"):
+        tax1 = Decimal("16000")+(cinc1-Decimal("200000"))*Decimal("0.17")
+    # Person 1 Tax Payable
+
+    if cinc2 <= Decimal("0"):
+        tax2 = Decimal("0")
+    elif cinc2 > Decimal("0") and cinc2 <= Decimal("50000"):
+        tax2 = cinc2*Decimal("0.02")
+    elif cinc2 > Decimal("50000") and cinc2 <= Decimal("100000"):
+        tax2 = Decimal("1000")+(cinc2-Decimal("50000"))*Decimal("0.06")
+    elif cinc2 > Decimal("100000") and cinc2 <= Decimal("150000"):
+        tax2 = Decimal("4000")+(cinc2-Decimal("100000"))*Decimal("0.1")
+    elif cinc2 > Decimal("150000") and cinc2 <= Decimal("200000"):
+        tax2 = Decimal("9000")+(cinc2-Decimal("150000"))*Decimal("0.14")
+    elif cinc2 > Decimal("200000"):
+        tax2 = Decimal("16000")+(cinc2-Decimal("200000"))*Decimal("0.17")
+    # Person 2 Tax Payable
+
+    if cjinc <= Decimal("0"):
+        jtax = Decimal("0")
+    elif cjinc > Decimal("0") and cjinc <= Decimal("50000"):
+        jtax = cjinc*Decimal("0.02")
+    elif cjinc > Decimal("50000") and cjinc <= Decimal("100000"):
+        jtax = Decimal("1000")+(cjinc-Decimal("50000"))*Decimal("0.06")
+    elif cjinc > Decimal("100000") and cjinc <= Decimal("150000"):
+        jtax = Decimal("4000")+(cjinc-Decimal("100000"))*Decimal("0.1")
+    elif cjinc > Decimal("150000") and cjinc <= Decimal("200000"):
+        jtax = Decimal("9000")+(cjinc-Decimal("150000"))*Decimal("0.14")
+    elif cjinc > Decimal("200000"):
+        jtax = Decimal("16000")+(cjinc-Decimal("200000"))*Decimal("0.17")
+    # Joint Tax Payable
+
+    stax1 = (inc1-mpf1)*Decimal("0.15")
+    stax2 = (inc2-mpf2)*Decimal("0.15")
+    sjtax = (jinc-jmpf)*Decimal("0.15")
+
+    if stax1 < tax1:
+        tax1 = stax1
+    if stax2 < tax2:
+        tax2 = stax2
+    if sjtax < jtax:
+        jtax = sjtax
+
+    btax = tax1 + tax2
+    # Person 1 + Person 2 Tax Payable
+
+    print()
+    print_row("", "Person 1", "Person 2", "Joint Assessment")
+    print_row("Total Income:", round(inc1, 2), round(inc2, 2), round(jinc, 2))
+    print_row("Deduction (MPF):", round(mpf1, 2),
+              round(mpf2, 2), round(jmpf, 2))
+    print_row("Basic/Married Person's Allowances:",
+              round(basicA, 2), round(basicA, 2), round(mpA, 2))
+    print_row("Net Chargeable Income:", round(
+        cinc1, 2), round(cinc2, 2), round(cjinc, 2))
+    print_row("Tax Payable:", round(
+        tax1, 2), round(tax2, 2), round(jtax, 2))
+    print("%-45s %27s %28s" % ("Total Tax Payable By Both:",
+          round(btax, 2), round(jtax, 2)))
+    print()
+    if btax > jtax:
+        print("As seen from above calculations, a Joint Assessment will result in tax savings amounting to $"+str(round((btax-jtax), 2)
+                                                                                                                  )+" ($"+str(round(btax, 2))+" - $"+str(round(jtax, 2))+"), and as such a Joint Assessment is recommended.")
+    elif btax < jtax:
+        print("As seen from above calculations, Individual Assessments will result in tax savings amounting to $"+str(round((jtax-btax), 2)
+                                                                                                                      )+" ($"+str(round(jtax, 2))+" - $"+str(round(btax, 2))+"), and as such Individual Assessments are recommended.")
+    elif btax == jtax:
+        print("As seen from above calculations, there are no savings between a Joint Assessment or Individual Assessments ($" +
+              str(round(btax, 2))+" = $"+str(round(jtax, 2))+"), and as such either assessment methods can be used.")
+
+    # check if user wants another calculation
+    next_calc = input(
+        '\nType "exit" to end program, otherwise type anything to restart: ')
+    return next_calc, mpf1, mpf2, jtax, btax
+
+
+# Due to how error is handled in input 1 and input 2, with a while loop to handle exceptions, we have yet to find ways to perform pytest for the raised exceptions
+
+# def test_InvalidOperation1(monkeypatch):
+#     with pytest.raises(InvalidOperation):
+#         input1()
+#         monkeypatch.setattr('builtins.input', lambda _: "notanumber")
+
+# def test_LessThanZeroException1(monkeypatch):
+#     with pytest.raises(LessThanZeroException):
+#         input1()
+#         monkeypatch.setattr('builtins.input', lambda _: "-12345")
+
+def test_0(monkeypatch):
+    errors = []
+
+    monkeypatch.setattr('builtins.input', lambda _: "exit")
+    next_calc, mpf1, mpf2, jtax, btax = main(Decimal("0"), Decimal("0"))
+
+    # replace assertions by conditions
+    if not mpf1 == Decimal("0"):
+        errors.append("Person 1 MPF != 0")
+    if not mpf2 == Decimal("0"):
+        errors.append("Person 2 MPF != 0")
+    if not btax == Decimal("0"):
+        errors.append("Combined Tax Payable != 0")
+    if not jtax == Decimal("0"):
+        errors.append("Joint Tax Payable != 0")
+
+    # assert no error message has been registered, else print messages
+    assert not errors, "Errors occured:\n{}".format("\n".join(errors))
+
+
+def test_nompf(monkeypatch):
+    errors = []
+
+    monkeypatch.setattr('builtins.input', lambda _: "exit")
+    next_calc, mpf1, mpf2, jtax, btax = main(
+        Decimal("85199.99"), Decimal("85199.99"))
+
+    # replace assertions by conditions
+    if not mpf1 == Decimal("0"):
+        errors.append("Person 1 MPF != 0")
+    if not mpf2 == Decimal("0"):
+        errors.append("Person 2 MPF != 0")
+    if not btax == Decimal("0"):
+        errors.append("Combined Tax Payable != 0")
+    if not jtax == Decimal("0"):
+        errors.append("Joint Tax Payable != 0")
+
+    # assert no error message has been registered, else print messages
+    assert not errors, "Errors occured:\n{}".format("\n".join(errors))
+
+
+def test_basempf(monkeypatch):
+    errors = []
+
+    monkeypatch.setattr('builtins.input', lambda _: "exit")
+    next_calc, mpf1, mpf2, jtax, btax = main(
+        Decimal("85200"), Decimal("85200"))
+
+    # replace assertions by conditions
+    if not mpf1 == Decimal("4260"):
+        errors.append("Person 1 MPF != 4260")
+    if not mpf2 == Decimal("4260"):
+        errors.append("Person 2 MPF != 4260")
+    if not btax == Decimal("0"):
+        errors.append("Combined Tax Payable != 0")
+    if not jtax == Decimal("0"):
+        errors.append("Joint Tax Payable != 0")
+
+    # assert no error message has been registered, else print messages
+    assert not errors, "Errors occured:\n{}".format("\n".join(errors))
+
+
+def test_maxmpf(monkeypatch):
+    errors = []
+
+    monkeypatch.setattr('builtins.input', lambda _: "exit")
+    next_calc, mpf1, mpf2, jtax, btax = main(
+        Decimal("360000"), Decimal("360000"))
+
+    # replace assertions by conditions
+    if not mpf1 == Decimal("18000"):
+        errors.append("Person 1 MPF != 18000")
+    if not mpf2 == Decimal("18000"):
+        errors.append("Person 2 MPF != 18000")
+    if not btax == Decimal("35400"):
+        errors.append("Combined Tax Payable != 35400")
+    if not jtax == Decimal("53400"):
+        errors.append("Joint Tax Payable != 53400")
+
+    # assert no error message has been registered, else print messages
+    assert not errors, "Errors occured:\n{}".format("\n".join(errors))
+
+
+def test_maxnotax(monkeypatch):
+    errors = []
+
+    monkeypatch.setattr('builtins.input', lambda _: "exit")
+    next_calc, mpf1, mpf2, jtax, btax = main(
+        Decimal("138947.36"), Decimal("138947.36"))
+
+    # replace assertions by conditions
+    if not round(mpf1, 2) == Decimal("6947.37"):
+        errors.append("Person 1 MPF != 6947.37")
+    if not round(mpf2, 2) == Decimal("6947.37"):
+        errors.append("Person 2 MPF != 6947.37")
+    if not round(btax, 2) == Decimal("0"):
+        errors.append("Combined Tax Payable != 0")
+    if not round(jtax, 2) == Decimal("0"):
+        errors.append("Joint Tax Payable != 0")
+
+    # assert no error message has been registered, else print messages
+    assert not errors, "Errors occured:\n{}".format("\n".join(errors))
+
+
+def test_taxbracket1(monkeypatch):
+    errors = []
+
+    monkeypatch.setattr('builtins.input', lambda _: "exit")
+    next_calc, mpf1, mpf2, jtax, btax = main(
+        Decimal("165263.15"), Decimal("165263.15"))
+
+    # replace assertions by conditions
+    if not round(mpf1, 2) == Decimal("8263.16"):
+        errors.append("Person 1 MPF != 8263.16")
+    if not round(mpf2, 2) == Decimal("8263.16"):
+        errors.append("Person 2 MPF != 8263.16")
+    if not round(btax, 2) == Decimal("1000"):
+        errors.append("Combined Tax Payable != 1000")
+    if not round(jtax, 2) == Decimal("1000"):
+        errors.append("Joint Tax Payable != 1000")
+
+    # assert no error message has been registered, else print messages
+    assert not errors, "Errors occured:\n{}".format("\n".join(errors))
+
+
+def test_taxbracket2(monkeypatch):
+    errors = []
+
+    monkeypatch.setattr('builtins.input', lambda _: "exit")
+    next_calc, mpf1, mpf2, jtax, btax = main(
+        Decimal("191578.94"), Decimal("191578.94"))
+
+    # replace assertions by conditions
+    if not round(mpf1, 2) == Decimal("9578.95"):
+        errors.append("Person 1 MPF != 9578.95")
+    if not round(mpf2, 2) == Decimal("9578.95"):
+        errors.append("Person 2 MPF != 9578.95")
+    if not round(btax, 2) == Decimal("2000"):
+        errors.append("Combined Tax Payable != 2000")
+    if not round(jtax, 2) == Decimal("4000"):
+        errors.append("Joint Tax Payable != 4000")
+
+    # assert no error message has been registered, else print messages
+    assert not errors, "Errors occured:\n{}".format("\n".join(errors))
+
+
+def test_taxbracket3(monkeypatch):
+    errors = []
+
+    monkeypatch.setattr('builtins.input', lambda _: "exit")
+    next_calc, mpf1, mpf2, jtax, btax = main(
+        Decimal("217894.73"), Decimal("217894.73"))
+
+    # replace assertions by conditions
+    if not round(mpf1, 2) == Decimal("10894.74"):
+        errors.append("Person 1 MPF != 10894.74")
+    if not round(mpf2, 2) == Decimal("10894.74"):
+        errors.append("Person 2 MPF != 10894.74")
+    if not round(btax, 2) == Decimal("5000"):
+        errors.append("Combined Tax Payable != 5000")
+    if not round(jtax, 2) == Decimal("9000"):
+        errors.append("Joint Tax Payable != 9000")
+
+    # assert no error message has been registered, else print messages
+    assert not errors, "Errors occured:\n{}".format("\n".join(errors))
+
+
+def test_taxbracket4(monkeypatch):
+    errors = []
+
+    monkeypatch.setattr('builtins.input', lambda _: "exit")
+    next_calc, mpf1, mpf2, jtax, btax = main(
+        Decimal("244210.52"), Decimal("244210.52"))
+
+    # replace assertions by conditions
+    if not round(mpf1, 2) == Decimal("12210.53"):
+        errors.append("Person 1 MPF != 12210.53")
+    if not round(mpf2, 2) == Decimal("12210.53"):
+        errors.append("Person 2 MPF != 12210.53")
+    if not round(btax, 2) == Decimal("8000"):
+        errors.append("Combined Tax Payable != 8000")
+    if not round(jtax, 2) == Decimal("16000"):
+        errors.append("Joint Tax Payable != 16000")
+
+    # assert no error message has been registered, else print messages
+    assert not errors, "Errors occured:\n{}".format("\n".join(errors))
+
+
+def test_chan(monkeypatch):
+    errors = []
+
+    monkeypatch.setattr('builtins.input', lambda _: "exit")
+    next_calc, mpf1, mpf2, jtax, btax = main(
+        Decimal("540000"), Decimal("120000"))
+
+    # replace assertions by conditions
+    if not round(mpf1, 2) == Decimal("18000"):
+        errors.append("Person 1 MPF != 18000")
+    if not round(mpf2, 2) == Decimal("6000"):
+        errors.append("Person 2 MPF != 6000")
+    if not round(btax, 2) == Decimal("48300"):
+        errors.append("Combined Tax Payable != 48300")
+    if not round(jtax, 2) == Decimal("45240"):
+        errors.append("Joint Tax Payable != 45240")
+
+    # assert no error message has been registered, else print messages
+    assert not errors, "Errors occured:\n{}".format("\n".join(errors))
+
+
+def test_chan2(monkeypatch):
+    errors = []
+
+    monkeypatch.setattr('builtins.input', lambda _: "exit")
+    next_calc, mpf1, mpf2, jtax, btax = main(
+        Decimal("540000"), Decimal("420000"))
+
+    # replace assertions by conditions
+    if not round(mpf1, 2) == Decimal("18000"):
+        errors.append("Person 1 MPF != 18000")
+    if not round(mpf2, 2) == Decimal("18000"):
+        errors.append("Person 2 MPF != 18000")
+    if not round(btax, 2) == Decimal("76200"):
+        errors.append("Combined Tax Payable != 76200")
+    if not round(jtax, 2) == Decimal("94200"):
+        errors.append("Joint Tax Payable != 94200")
+
+    # assert no error message has been registered, else print messages
+    assert not errors, "Errors occured:\n{}".format("\n".join(errors))
+
+
+def test_stdtax1(monkeypatch):
+    errors = []
+
+    monkeypatch.setattr('builtins.input', lambda _: "exit")
+    next_calc, mpf1, mpf2, jtax, btax = main(
+        Decimal("3500000"), Decimal("5000000"))
+
+    # replace assertions by conditions
+    if not round(mpf1, 2) == Decimal("18000"):
+        errors.append("Person 1 MPF != 18000")
+    if not round(mpf2, 2) == Decimal("18000"):
+        errors.append("Person 2 MPF != 18000")
+    if not round(btax, 2) == Decimal("1269600"):
+        errors.append("Combined Tax Payable != 1269600")
+    if not round(jtax, 2) == Decimal("1269600"):
+        errors.append("Joint Tax Payable != 1269600")
+
+    # assert no error message has been registered, else print messages
+    assert not errors, "Errors occured:\n{}".format("\n".join(errors))
+
+
+def test_stdtax2(monkeypatch):
+    errors = []
+
+    monkeypatch.setattr('builtins.input', lambda _: "exit")
+    next_calc, mpf1, mpf2, jtax, btax = main(
+        Decimal("2500000"), Decimal("1800000"))
+
+    # replace assertions by conditions
+    if not round(mpf1, 2) == Decimal("18000"):
+        errors.append("Person 1 MPF != 18000")
+    if not round(mpf2, 2) == Decimal("18000"):
+        errors.append("Person 2 MPF != 18000")
+    if not round(btax, 2) == Decimal("634800"):
+        errors.append("Combined Tax Payable != 634800")
+    if not round(jtax, 2) == Decimal("639600"):
+        errors.append("Joint Tax Payable != 639600")
+
+    # assert no error message has been registered, else print messages
+    assert not errors, "Errors occured:\n{}".format("\n".join(errors))
+
+
+if __name__ == "__main__":
+    intro()
+    while True:
+        brk()
+        next_calc, mpf1, mpf2, jtax, btax = main(input1(), input2())
+        if next_calc == "exit":
+            break
